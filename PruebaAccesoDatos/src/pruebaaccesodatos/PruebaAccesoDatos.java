4689643653bf4330944b0aa796699c8eb0ed7907
alsebher <114057137+alsebher@users.noreply.github.com>
2022-09-26 10:58:17 +0200

    SubidaHolaMundo

---
+++ PruebaAccesoDatos/src/pruebaaccesodatos/PruebaAccesoDatos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaaccesodatos;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class PruebaAccesoDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("\tHOLA MUNDO SOY ALBERTO");
+        System.out.println("=============================================");
+        System.out.println("");
+    }
+    
+}
